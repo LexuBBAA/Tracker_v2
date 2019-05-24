27e2f06dc8c58020f24e4bd15360911fc7cba630
lexubbaa <a.birsasteanu@gmail.com>
2019-05-24 22:30:37 +0300

    Added base configuration
    1. Migrated app/gradle.build to the new AndroidX
    2. Added base AndroidX libraries to the project
    3. Added google-services.json for the Firebase App

--- android/app/src/main/java/com/tracker/trackerv2/MainActivity.kt
+++ android/app/src/main/java/com/tracker/trackerv2/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.tracker.trackerv2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener { result ->
+            Log.i(MainActivity::class.simpleName, "Token: ${result.token}")
+        }
     }
 
 }
