59d41173af2ae72843b065c55f541e6226c3a348
Bogdan Birsasteanu <a.birsasteanu@gmail.com>
2019-07-11 01:00:02 +0300

    Added Firebase SDK and checked subscription

--- android/app/src/main/java/com/tracker/trackerv2/MainActivity.kt
+++ android/app/src/main/java/com/tracker/trackerv2/MainActivity.kt
@@ -2,12 +2,24 @@ package com.tracker.trackerv2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener { task ->
+            if(!task.isSuccessful) {
+                Log.e(MainActivity::class.java.simpleName, "FCM failed to generate deviceId", task.exception)
+                return@addOnCompleteListener
+            }
+
+            Log.e(this::class.java.simpleName, ">>> DeviceId: ")
+            Log.e(this::class.java.simpleName, task.result?.token ?: "null")
+        }
     }
 
 }
